daa1646a5b48d2d9b184410dd6e1cd52cf8ce562
JonathanVK1 <jonathanvictoria41@gmail.com>
2024-05-26 23:08:07 -0300

    corregi un par de lineas pero aun continua el error

--- main2.c
+++ main2.c
@@ -7,14 +7,20 @@
 
 int main(){
     FILE *f=fopen ("swt_data.dat","rb");
+    int index,type;
     uint32_t header;
     float data;
-    int index=(header <<2)>>2;
-    int type=header >>30;
     struct v V;
+    while(!feof(f)){
+        fread(&header,sizeof(uint32_t),1,f);
+        fread(&data,sizeof(float),1,f);
+        index=(header <<2)>>2;
+        type=header >>30;
+        
+        ord(index,type,data,V);
 
-    ord(index,type,data,V);
+        imp(V);
 
-    imp(V);
-
+    }
+    
 }
